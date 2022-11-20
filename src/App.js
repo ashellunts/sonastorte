ae6d470afe758e07d4b18e1fa4610715b27a4668
Artur Shellunts <artur.shellunts@profiforms.de>
2022-11-20 16:53:45 +0100

    add photo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import sona from './sona.jpg'
+
+
 function App() {
   return (
     <div>
-      <h1>Sonas Torte v2</h1>
+      <h1>Sonas Torte</h1>
+      <img alt="Sona" width={200} src={sona}/>
     </div>
   );
 }
