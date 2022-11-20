fd6a771aa3c683602fa85456c36b08dc240c80e9
Artur Shellunts <artur.shellunts@profiforms.de>
2022-11-20 18:32:55 +0100

    update logo and title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import sona from './sona.jpg'
+import logo from './logo.png'
 
 
 function App() {
   return (
     <div>
-      <h1>Sonas Torte</h1>
+      <img alt="Logo" width={300} src={logo}/>
+      <br/>
+      <br/>
+      <br/>
       <img alt="Sona" width={200} src={sona}/>
     </div>
   );
