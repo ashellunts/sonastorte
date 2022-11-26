1db9ed507621f273472c883db14b51a5b502d8a0
Artur Shellunts <shellunts.artur@gmail.com>
2022-11-26 22:59:01 +0100

    add navigation

--- src/App.js
+++ src/App.js
@@ -1,51 +1,131 @@
+import React from 'react';
 import { useState } from 'react'
 
 const fillings = {
-    name: "Начинки",
+    name: 'Начинки',
     items: [
         'клюква',
         'малина',
-        'клубника'
+        'клубника',
+        '🍫'
     ]
 }
 
 const fillings2 = {
-    name: "Начинки 2",
+    name: 'Бисквит',
     items: [
-        'клюква 1',
-        'малина 1',
-        'клубника 1'
+        '🍫',
+        'белый',
+        'фисташка'
     ]
 }
 
-const ingredients = [fillings, fillings2]
+const fillings3 = {
+    name: 'Крем',
+    items: [
+        '🍫',
+        'клубничный',
+        'малиновый'
+    ]
+}
 
-function Ingredients(data, ingredientChosen) {
+const ingredients = [fillings, fillings2, fillings3]
+
+function IngredientSelector(ingredients, chosenIngredient, setChosenIngredient) {
     return (
-        <>
-            <h1>{data.name}</h1>
-            {
-                data.items.map(filling => (
-                    <button onClick={() => ingredientChosen(filling)}>{filling}</button>
-                ))
-            }
-        </>
+        <div>
+            <div>
+                <h2>{ingredients.name}</h2>
+                {
+                    ingredients.items.map((filling, index) => (
+                        <button key={index} onClick={() => setChosenIngredient(filling)}>{filling}</button>
+                    ))
+                }
+            </div>
+            <p>Выбранный ингредиент: {chosenIngredient}</p>
+        </div>
     )
 }
 
 function App() {
-    const [chosenIngredient, setChosenIngredient] = useState('')
+    const [chosenIngredients, setChosenIngredients] = useState([])
+    const [currentIngredient, setCurrentIngredient] = useState(-2)
+    const [name, setName] = useState('')
+    const [address, setAddress] = useState('')
 
-    function ingredientChosen(ingredient) {
-        setChosenIngredient(ingredient)
+
+    function setChosenIngredient(index) {
+        return (chosenIngredient) => {
+            const newChosenIngredients = chosenIngredients.slice()
+            newChosenIngredients[index] = chosenIngredient
+            setChosenIngredients(newChosenIngredients)
+            next()
+        }
+    }
+
+    function next() {
+        if (currentIngredient !== ingredients.length) {
+            setCurrentIngredient(currentIngredient + 1)
+        }
+    }
+
+    function back() {
+        if (currentIngredient !== -2) {
+            setCurrentIngredient(currentIngredient - 1)
+        }
     }
 
     return (
         <div>
-            {Ingredients(ingredients[0], ingredientChosen)}
-            <p>Выбранный ингредиент: {chosenIngredient}</p>
-        </div >
+            {currentIngredient >= 0 ?
+                <div>
+                    <b> Выбранные игредиенты </b>
+                    {
+                        ingredients
+                            .map((ingredient, index) =>
+                                <div key={index}>
+                                    <i>{ingredient.name}:</i> {chosenIngredients[index]}
+                                </div>
+                            )
+                    }
+                </div>
+                : null
+            }
+            {currentIngredient === ingredients.length
+                ?
+                <div>
+                    <p><b>Ваше имя:</b> {name}</p>
+                    <p><b>Адрес:</b> {address}</p>
+                </div>
+                : currentIngredient === -2
+                    ?
+                    <div>
+                        <p>Ваше имя</p>
+                        <input value={name} onChange={event => { setName(event.target.value) }} />
+                        <button onClick={() => { next() }} >далее</button>
+                    </div>
+                    : currentIngredient === -1
+                        ?
+                        <div>
+                            <p>Адрес</p>
+                            <input value={address} onChange={event => { setAddress(event.target.value) }} />
+                            <button onClick={() => { next() }} >далее</button>
+                        </div>
+                        : IngredientSelector(
+                            ingredients[currentIngredient],
+                            chosenIngredients[currentIngredient],
+                            setChosenIngredient(currentIngredient))
+            }
+            {currentIngredient !== -2
+                ? <button onClick={() => { back() }}>назад</button>
+                : null
+            }
+            {/* {currentIngredient !== ingredients.length
+                ? <button onClick={() => { next() }} >далее</button>
+                : null
+            } */}
+        </div>
     )
 }
 
-export default App;
+export default App
--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import logo from './logo.png'
 
 function Footer() {
     return (
-      <div>
-        <img alt="Logo" width={300} src={logo}/>
-      </div>
+        <div>
+            <img alt="Logo" width={300} src={logo} />
+        </div>
     )
 }
 
