faaec56ea8693069e16e32e93724eb91da281d82
Artur Shellunts <artur.shellunts@profiforms.de>
2022-11-28 19:38:59 +0100

    add tags to DecorExplorer

--- src/DecorExplorer.js
+++ src/DecorExplorer.js
@@ -1,17 +1,43 @@
 import React from 'react';
 
 const decorsList = [
-    "русалка",
-    "машинки",
-    "футбол",
-    "дисней"
+    {
+        name: "русалка",
+        tags: ["tag1", "tag2"]
+    },
+    {
+        name: "машинки",
+        tags: ["tag1"]
+    },
+    {
+        name: "футбол",
+        tags: ["tag1"]
+    },
+    {
+        name: "дисней",
+        tags: ["tag1"]
+    }
 ]
 
 function DecorsExplorer() {
     return (
         <div>
             {decorsList.map((decor, index) => (
-                <p key={index}>{decor}</p>
+                <div key={index}>
+                    <b>{decor.name}</b>
+                    <Tags tagList={decor.tags} />
+                    <br />
+                </div>
+            ))}
+        </div>
+    )
+}
+
+function Tags({ tagList }) {
+    return (
+        <div>
+            {tagList.map((tagName, index) => (
+                <i key={index}>#{tagName} </i>
             ))}
         </div>
     )
